c253caab05d3240c5177e5e6d430c18ae2a1c4a6
Pradyun Gedam <pradyunsg@gmail.com>
2019-04-04 15:28:53 +0530

    Update x86-64-jos-elf-binutils.rb

--- x86-64-jos-elf-binutils.rb
+++ x86-64-jos-elf-binutils.rb
@@ -1,4 +1,4 @@
-class I386JosElfBinutils < Formula
+class X8664JosElfBinutils < Formula
   homepage "http://pdos.csail.mit.edu/6.828/2014/tools.html"
   url "http://ftpmirror.gnu.org/binutils/binutils-2.21.1.tar.bz2"
   sha256 "cdecfa69f02aa7b05fbcdf678e33137151f361313b2f3e48aba925f64eabf654"
